2ddde2ad280892e60a046e93cb8ab2ca8186cdb4
Eatha Surya Prajyesh <121669038+suryatenz@users.noreply.github.com>
2024-01-23 09:55:35 +0530

    jj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    content:[
+      './pages/**/*.{html,js}',
+      './components/**/*.{html,js}',
+    ],
   theme: {
     extend: {},
   },
